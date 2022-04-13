0bac37d9d3f4f6e54df8d3028d1efc8dc9d03738
Emersyn Harris <eharr21@u.rochester.edu>
2022-04-13 11:06:01 -0400

    problem six

---
+++ 1-10/ProblemSix.java
@@ -0,0 +1,17 @@
+import java.lang.Math;
+
+public class ProblemSix{
+    public static void main(String[] args){
+        //sum of squares
+        double sumOfSquares = 0;
+        for(int i = 1; i <= 100; i++){
+            sumOfSquares += Math.pow(i, 2);
+        }
+        //square of the sum
+        int squareOfSum = 0;
+        for(int i = 1; i <= 100; i++){
+            squareOfSum += i;
+        }
+        System.out.println(Math.pow(squareOfSum, 2) - sumOfSquares);
+    }
+}
